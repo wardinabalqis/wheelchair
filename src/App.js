3bc1cb0a597115080ab0d7e623be6558b75dc6bc
zeroprojects17 <your-github-email@example.com>
2026-06-30 23:54:59 +0800

    pinggy

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
   const [mode, setMode] = useState("wifi");
 
   const CCTV_URL =
-    "https://spongy-pawing-cornea.ngrok-free.dev/?action=stream";
+    "https://snide-surpass-ongoing.ngrok-free.dev/?action=stream";
 
   // ================= AUTO LOGIN =================
   useEffect(() => {
